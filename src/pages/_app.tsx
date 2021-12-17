c00fd03d19e7567f311f08dbe182b58bd882bf39
marlon Felipe Passos <1marlonpassos@gmail.com>
2021-12-16 23:45:06 -0300

    add SessionProvider para saber user login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from 'next/app'; // Define os tipos de props do App
 import '../styles/global.scss';
+import { SessionProvider } from 'next-auth/react';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={ pageProps.section}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
